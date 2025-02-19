29d6cf4f6f56838aedb3a077eaf4e4a2280a1261
akshatkumar <akshatk806@gmail.com>
2025-02-19 14:03:22 +0530

    adjusting the reducer for our effects

--- src/app/books/book.reducer.ts
+++ src/app/books/book.reducer.ts
@@ -1,7 +1,7 @@
 import { createReducer, on } from "@ngrx/store";
 
 // reducer processing actions
-import { AddBook, RemoveBook } from "./book.action";
+import { AddBook, RemoveBook, AddBookSuccess, AddBookFailure } from "./book.action";
 
 import { Book } from "../models/book";  
 // But why reducer needs to know about book? -> most important
@@ -17,8 +17,22 @@ export const initialState: Book[] = [];
 export const BookReducer = createReducer(
     initialState,
     // registering two actions in our reducer
-    on(AddBook, (state, { id, title, author }) => [...state, { id, title, author }]),     // ... -> create a new array and copy all elements from old state into it   
+    on(AddBook, (state) => { return state }),      // the AddBook initial action only returns a state, but the effect will take care of starting of AddBookSuccess action once the service replied yes I added the book
     // we are registering a kind of listener for our AddBook action and once this action appears now we have to define what the reducer should realy do? we use spread operator to take all books from previous state(copy) finally add a new book [...state, { id, title, author }] -> this is new book 
     
+    on(AddBookSuccess, (state, { id, title, author }) => [...state, { id, title, author }]),      // ... -> create a new array and copy all elements from old state into it   
+    on(AddBookFailure, (state, { error }) => {              // { error } from props 
+        console.error(error); 
+        return state;
+    }),
+
     on(RemoveBook, (state, { bookId }) => state.filter(book => book.id !== bookId))  // results into new array
 );
+
+/* working :-
+The AddBook action will still gets dispatched from component, once we entered some inputs in out input fields
+in UI and we click on 'add book' we dispatch AddBook action which gets handles by the reducer and 
+The effect will notice there has been AddBook action dispatched and that moment it will start communicating
+from the service and waiting for the response and depending on that we will start AddBookSuccess action or 
+AddBookFailure from that effect
+*/
