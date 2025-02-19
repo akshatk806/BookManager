bb78aefcfec2c2ce6c2640112e392fe03c997af5
akshatkumar <akshatk806@gmail.com>
2025-02-19 13:52:19 +0530

    added success and failure actions

--- src/app/books/book.action.ts
+++ src/app/books/book.action.ts
@@ -2,7 +2,11 @@ import { createAction, props } from "@ngrx/store";   // sometimes we create a ac
 import { Book } from "../models/book";
 
 // creating constants
-export const AddBook = createAction('[Book] Add Book', props<Book>())   // we are going to submit books to this action. props means extra information, here props -> here is the Book you should create 
+export const AddBook = createAction('[Book] Add Book', props<Book>())   // we are going to submit books to this action. props means extra information, here props -> here is the Book you should create. [Book] is entity
+
+// AddBook is an initial action
+export const AddBookSuccess = createAction('[Book] Add Book Successfully', props<Book>()); // props -> this book has been created
+export const AddBookFailure = createAction('[Book] Add Book Failure', props<{error: any}>());
 
 // action for removing book
 export const RemoveBook = createAction('[Book] Remove Book', props<{bookId: string}>())     // props -> here is bookId you should remove the book
