d1873a9bdd525df287c4099ac22ec1d5387e3bd9
akshatkumar <akshatk806@gmail.com>
2025-02-19 21:28:20 +0530

    registering the effect

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,8 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BookListComponent } from './book-list/book-list.component';
 import { AppState } from './app.state';
+import { EffectsModule } from '@ngrx/effects';
+import { BookEffects } from './books/book.effects';
 
 @NgModule({
   declarations: [
@@ -15,7 +17,8 @@ import { AppState } from './app.state';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    StoreModule.forRoot<AppState>({ book: BookReducer })    // telling the store to use book reducer to manage all state changes related to books
+    StoreModule.forRoot<AppState>({ book: BookReducer }),   // telling the store to use book reducer to manage all state changes related to books
+    EffectsModule.forRoot([BookEffects])
   ],
   providers: [],
   bootstrap: [AppComponent]
